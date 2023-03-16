65fce2b5567e32c0e1735f189a599e33ea5f207a
martinbolsnes <martinbolsnes@icloud.com>
2023-03-16 11:23:23 +0100

    add styles about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,24 @@
+import { Container, createStyles, rem } from '@mantine/core';
 import { HeaderResponsive } from '../components/Header/Header';
 import { About } from '../components/about/About';
 import { TechInfo } from '../components/techInfo/TechInfo';
 import { Footer } from '../components/footer/Footer';
 import { CustomDivider } from '../components/customDivider/CustomDivider';
 
+const useStyles = createStyles((theme) => ({
+  about: {
+    marginTop: `${rem(180)}`,
+    maxWidth: `${rem(1280)}`,
+    paddingLeft: 0,
+
+    [theme.fn.smallerThan('sm')]: {
+      marginTop: `${rem(-80)}`,
+    },
+  },
+}));
+
 export default function AboutPage() {
+  const { classes } = useStyles();
   return (
     <>
       <HeaderResponsive
@@ -15,7 +29,9 @@ export default function AboutPage() {
           { link: '/contact', label: 'Contact' },
         ]}
       />
-      <About />
+      <Container className={classes.about}>
+        <About />
+      </Container>
       <CustomDivider />
       <TechInfo />
       <Footer />

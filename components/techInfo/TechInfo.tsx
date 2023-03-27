fcdaab6049465817b38acaba4089adf557b782a0
martinbolsnes <martinbolsnes@icloud.com>
2023-03-27 14:32:00 +0200

    update techInfo

--- components/techInfo/TechInfo.tsx
+++ components/techInfo/TechInfo.tsx
@@ -21,12 +21,12 @@ export function TechInfo() {
             Through my studies at Noroff I gathered most experience in HTML, CSS
             and Javascript. My main focus now is React and often through
             Next.js. As well as creating responsive applications through pure
-            CSS, I also have worked with frameworks like Bootstrap and Tailwind.
-            I do most of my designs in Figma, but I have also been working with
-            Adobe XD. I like to stay updated on different tools and countinue to
-            learn. Some of the things I do want to learn more about is
-            Typescript. I have also worked with React Native, Firebase, Strapi
-            and Sanity.
+            CSS, I also have worked with frameworks like Bootstrap, Tailwind and
+            Mantine. I do most of my designs in Figma, but I have also been
+            working with Adobe XD. I like to stay updated on different tools and
+            countinue to learn. Some of the things I do want to learn more about
+            is Typescript. I have also worked with React Native, Firebase,
+            Strapi and Sanity.
           </Text>
         </motion.div>
       </Container>
